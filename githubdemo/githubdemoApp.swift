d12e14bf4545e63a1166eb630f0dd30c2a41c6fa
sansawinterfell <2022-01508@sanbeda.edu.ph>
2023-03-22 14:08:26 +0800

    Initial Commit

---
+++ githubdemo/githubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemoApp.swift
+//  githubdemo
+//
+//  Created by Katrina Hallare on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct githubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
